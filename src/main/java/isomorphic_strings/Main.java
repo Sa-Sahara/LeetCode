29b9d4e2108135bc1e92f900356ac0e3c8b78012
Aleksandra Saharova <privet49069@gmail.com>
2023-07-21 18:19:51 +0600

    tasks solved on 20.07 & 21.07

---
+++ src/main/java/isomorphic_strings/Main.java
@@ -0,0 +1,11 @@
+package isomorphic_strings;
+
+public class Main {
+    public static void main(String[] args) {
+        IsomorphicStrings i = new IsomorphicStrings();
+        System.out.println(i.isIsomorphic("egg", "add"));
+        System.out.println(i.isIsomorphic("foo", "bar"));
+        System.out.println(i.isIsomorphic("paper", "title"));
+        System.out.println(i.isIsomorphic("badc", "baba"));
+    }
+}
